6ddf26bb56e16d5aed2fb0ca69c547d28758f253
Mohammed Sufyan <sufyan@ascendtis.com>
2023-07-15 11:56:28 +0530

    Publish-Package to NPM with Fix

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 export const useMediaQuery = (breakpoint?: string): boolean => {
   const [matches, setMatches] = React.useState(
-    !!breakpoint && window.matchMedia(breakpoint).matches,
+    !!breakpoint && (typeof window !== "undefined" && window.matchMedia(breakpoint).matches)
   );
-
   React.useEffect(() => {
     if (breakpoint) {
       const media = window.matchMedia(breakpoint);
